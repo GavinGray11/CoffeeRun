b63fb12fed631e38435f70b59174b7fae09a9e65
GavinGray11 <40133546+GavinGray11@users.noreply.github.com>
2021-10-12 21:32:25 -0700

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,20 +1,4 @@
 
-/*
-var FIREBASE_apiKey = "AIzaSyBPW7ltITohPEF3dg5ZOvOvDv-61i22iOo";
-var FIREBASE_authDomainapiKey = "coffeerun-3ba2e.firebaseapp.com";
-var FIREBASE_projectIdapiKey = "coffeerun-3ba2e";
-var FIREBASE_storageBucketapiKey = "coffeerun-3ba2e.appspot.com";
-var FIREBASE_messagingSenderIdapiKey = "22675998017";
-var FIREBASE_appIdapiKey = "1:22675998017:web:9556f7adadba07a2bc0dc0";
-*/
 
-var config = {
-  FIREBASE_apiKey:"AIzaSyBPW7ltITohPEF3dg5ZOvOvDv-61i22iOo",
-  FIREBASE_authDomainapiKey:"coffeerun-3ba2e.firebaseapp.com",
-  FIREBASE_projectIdapiKey : "coffeerun-3ba2e",
-  FIREBASE_storageBucketapiKey: "coffeerun-3ba2e.appspot.com",
-  FIREBASE_messagingSenderIdapiKey :"22675998017",
-  FIREBASE_appIdapiKey : "1:22675998017:web:9556f7adadba07a2bc0dc0",
-  }
 
 export default config;
